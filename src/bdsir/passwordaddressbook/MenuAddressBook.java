1cf38a066556aeaf3b37784da15ffef609bff052
Pasquale Polcaro <redlyon89@gmail.com>
2014-04-23 09:38:58 +0200

    v0.6
    Signed-off-by: Pasquale Polcaro <redlyon89@gmail.com>

--- src/bdsir/passwordaddressbook/MenuAddressBook.java
+++ src/bdsir/passwordaddressbook/MenuAddressBook.java
@@ -3,15 +3,17 @@ package bdsir.passwordaddressbook;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuAddressBook extends Activity
 {
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_password_1);
+		setContentView(R.layout.activity_menu);
 		
 		setPersonalFont();
 	}
